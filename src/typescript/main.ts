ac795edd308011fbd78e111e05ef110abf352a01
Abhishek Anil Deshmukh <deshmukh.abhishek369@gmail.com>
2020-09-11 22:42:49 +0530

    responsive and fonts done

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,8 +1,11 @@
-function A() {
-  const sections = document.getElementsByClassName('section');
+const loadLinks = () => {
+  const sections = document.getElementsByTagName('section');
   for (let i = 0; i < sections.length; i++) {
     sections[i].addEventListener('click', () => {
       window.location.replace(sections[i].getAttribute('data'));
     });
   }
 };
+document.addEventListener("DOMContentLoaded", function(event) {
+  loadLinks()
+});
